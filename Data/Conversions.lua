09f16e3631be205a5d89a1f70532cf685b09a78a
Artem Solovev <77993028+staticcole@users.noreply.github.com>
2026-07-16 19:25:04 +0200

    Add files via upload

---
+++ Data/Conversions.lua
@@ -0,0 +1,16 @@
+-- Generated file. Conversion tuple: {id, expansion, phase, reversible, inputs, outputs}.
+SPP_CONVERSIONS = {
+  {"essence-magic-up",1,1,1,{10938,3},{10939,1}},
+  {"essence-astral-up",1,1,1,{10998,3},{11082,1}},
+  {"essence-mystic-up",1,1,1,{11134,3},{11135,1}},
+  {"essence-nether-up",1,1,1,{11174,3},{11175,1}},
+  {"essence-eternal-up",1,1,1,{16202,3},{16203,1}},
+  {"essence-planar-up",2,1,1,{22447,3},{22446,1}},
+  {"primal-fire-up",2,1,1,{22574,10},{21884,1}},
+  {"primal-air-up",2,1,1,{22572,10},{22451,1}},
+  {"primal-water-up",2,1,1,{22578,10},{21885,1}},
+  {"primal-earth-up",2,1,1,{22573,10},{22452,1}},
+  {"primal-mana-up",2,1,1,{22576,10},{22457,1}},
+  {"primal-life-up",2,1,1,{22575,10},{21886,1}},
+  {"primal-shadow-up",2,1,1,{22577,10},{22456,1}},
+}
